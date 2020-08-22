84eed73acbb9687f582723081a8915e2adb950f2
Crazy <dmitrijs.braslis@gmail.com>
2020-08-22 20:53:22 +0300

    logger

--- src/test/java/BillMeTests.java
+++ src/test/java/BillMeTests.java
@@ -1,8 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 public class BillMeTests {
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
     @Test
     public void firstTest() {
+        LOGGER.info("Privet");
         System.out.println("Hello, World!");
     }
 }
