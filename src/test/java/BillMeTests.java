8bee922868b1e1ed48555a9fa2ef2c1f87f5602f
Crazy <dmitrijs.braslis@gmail.com>
2020-08-22 21:35:02 +0300

    test2

--- src/test/java/BillMeTests.java
+++ src/test/java/BillMeTests.java
@@ -1,11 +1,15 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BillMeTests {
     private final Logger LOGGER = LogManager.getLogger(this.getClass());
     @Test
     public void firstTest() {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         LOGGER.info("Privet");
         System.out.println("Hello, World!");
     }
